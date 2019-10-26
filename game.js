ee29fc3774b28968da6f36e30b771b02a1f19469
Helena Makrenek <hmakrenek@gmail.com>
2019-10-26 17:33:04 +0200

    object working version

--- game.js
+++ game.js
@@ -1,45 +1,50 @@
-const glasses = document.querySelector(".glasses");
-const shoes = document.querySelector(".shoes");
-const hat = document.querySelector(".hat");
-const bikini = document.querySelector(".bikini");
-const towel = document.querySelector(".towel");
-const sunscreen = document.querySelector(".sunscreen");
-const bagpack = document.querySelector(".bagpack");
-const medicine = document.querySelector(".medicine");
-const passport = document.querySelector(".passport");
-const map = document.querySelector(".map");
-const camera = document.querySelector(".camera");
-const watch = document.querySelector(".watch");
-const bottle = document.querySelector(".bottle");
-const sandwich = document.querySelector(".sandwich");
-const bag = document.querySelector(".bag");
-const radio = document.querySelector(".radio");
-const phone = document.querySelector(".phone");
+class Item {
+    constructor(name) {
+        this.name = name;
+        this.element = document.querySelector(`.${name}`);
 
+
+        const pickUp = this.pickUp.bind(this);
+        this.element.addEventListener("click", function () {
+            pickUp();
+            itemListShown.innerHTML = "";
+            printList();
+            handleWin();
+        })
+    }
+    activate() {
+        this.element.classList.add("box-blue");
+        activeItems.push(this);
+    }
+    pickUp() {
+        this.element.classList.add("erace");
+        activeItems.splice(activeItems.indexOf(this), 1);
+
+    }
+    getName() {
+        return this.name;
+    }
+};
 
 const itemListShown = document.querySelector(".itemList");
-const restartBtn = document.querySelector(".btn-restart");
 
-const itemList = [glasses, shoes, hat, bikini, towel, sunscreen, bagpack, medicine, passport, map, camera, watch, bottle, sandwich, bag, radio, phone];
+const itemList = [new Item("glasses"), new Item("shoes"), new Item("hat"), new Item("bikini"), new Item("towel"), new Item("sunscreen"), new Item("bagpack"), new Item("medicine")]
 
 let newItems = [...itemList];
 
 const activeItems = [];
 
-const activateItems = function () {
+(function activeItem() {
     for (let i = 0; i <= itemList.length / 2 - 1; i++) {
         let currentItem = Math.floor(Math.random() * newItems.length);
-        newItems[currentItem].classList.add("box-blue");
-        activeItems.push(newItems[currentItem]);
+        newItems[currentItem].activate();
         newItems.splice(currentItem, 1);
     }
-}
-
-activateItems();
+})()
 
 const printList = () => {
-    activeItems.forEach(element => {
-        itemListShown.innerHTML += `<p>${element.id}</p>`;
+    activeItems.forEach(item => {
+        itemListShown.innerHTML += `<p>${item.getName()}</p>`;
     })
 };
 
@@ -51,19 +56,3 @@ const handleWin = () => {
         Your score is: 0`);
     }
 };
-
-const eraceItem = function () {
-    activeItems.forEach(element => {
-        element.addEventListener("click", function () {
-            this.classList.add("erace");
-            indexNum = activeItems.indexOf(this);
-            activeItems.splice(indexNum, 1);
-            itemListShown.innerHTML = "";
-            printList();
-            handleWin();
-        })
-    })
-};
-
-eraceItem();
-           // handleLose(); - uzalezniony od czasu
